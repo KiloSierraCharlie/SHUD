030318386f29d8be206f31162b6e904b41ce4c65
Kieran Cross <mindfulhacker@users.noreply.github.com>
2015-06-25 13:49:39 +0100

    Create sv_startup.lua

---
+++ SHUD/sv_startup.lua
@@ -0,0 +1,10 @@
+MsgN(' ')
+MsgN(' ')
+MsgN('//////////////////////////////////')
+MsgN('//   Loaded SHUD DarkRP Module  //')
+MsgN('//       Version M-9 Stable     //')
+MsgN('//////////////////////////////////')
+MsgN(' ')
+MsgN(' ')
+
+CheckVersion()
